715c9801abdb58aedeca1f0c0edf809d23864a62
Terrence Bell <terrencekbell@gmail.com>
2020-03-06 20:49:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(array)
+array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort {|x,y| -(x <=> y)}
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array,index,destination_index)
+  array[index], array[index] = array[destination_index], array[destination_index]
+array
+end
+
+def reverse_array(array)
+  array.reverse
+end
